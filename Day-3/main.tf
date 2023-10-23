03fa69bcd566680a37445b3d36ff0d3561787838
Devendra-Yadav1982 <93039381+Devendra-Yadav1982@users.noreply.github.com>
2023-10-23 15:32:33 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
+  ami_value = "ami-0287a05f0ef0e9d9a" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  subnet_id_value = "subnet-07d213aa9a354b5dd". # replace this
 }
